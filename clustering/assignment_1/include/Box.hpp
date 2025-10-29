4e101e7543da8361d9f5a5a8ba9bb69faeec7a7b
Edanna\Davide <davide.gazzadi@gmail.com>
2025-10-29 22:42:09 +0100

    start of first assignement

---
+++ clustering/assignment_1/include/Box.hpp
@@ -0,0 +1,27 @@
+#include <Eigen/Geometry> 
+
+namespace lidar_obstacle_detection
+{
+
+  struct BoxQ
+  {
+    Eigen::Vector3f bbox_transform;
+    Eigen::Quaternionf bbox_quaternion;
+    float cube_length;
+    float cube_width;
+    float cube_height;
+  };
+
+  struct Box
+  {
+    int label;
+    float x_min;
+    float y_min;
+    float z_min;
+    float x_max;
+    float y_max;
+    float z_max;
+  };
+
+}
+
